f12dbb2a7f93cc28edebd24223ef4fa2c219dd29
Marina-28 <greatundeadmoose@gmail.com>
2022-05-04 16:58:01 +0200

    Creating the first row of aliens.

--- alien.py
+++ alien.py
@@ -8,7 +8,7 @@ class Alien(Sprite):
         super().__init__()
         self.screen = ai_game.screen
 
-        self.image = pygame.image.load('images/alien_.bmp')
+        self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.x = self.rect.width
